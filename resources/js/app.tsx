98bf4448d630c1b04ecaf8d341877817c91ec04f
Alan Sanchez <realalansan@gmail.com>
2022-07-06 19:30:29 -0700

    Replace default app with typescript

---
+++ resources/js/app.tsx
@@ -0,0 +1,31 @@
+import "./bootstrap";
+import "../css/app.css";
+
+import React from "react";
+import { render } from "react-dom";
+import { createInertiaApp } from "@inertiajs/inertia-react";
+import { InertiaProgress } from "@inertiajs/progress";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+
+declare global {
+    interface ImportMeta {
+        glob: any;
+    }
+}
+
+const appName =
+    window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob("./Pages/**/*.jsx")
+        ),
+    setup({ el, App, props }) {
+        return render(<App {...props} />, el);
+    },
+});
+
+InertiaProgress.init({ color: "#4B5563" });
