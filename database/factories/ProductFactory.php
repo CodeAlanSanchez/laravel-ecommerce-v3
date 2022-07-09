f5608cc396ac3d3667d3a3f98059f85fcc9434e1
Alan Sanchez <realalansan@gmail.com>
2022-07-09 16:41:49 -0700

    Add product seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->colorName(),
+            'description' => fake()->realTextBetween(),
+            'image_url' => fake()->url(),
+            'price' => fake()->numberBetween(0, 99999),
+        ];
+    }
+}
