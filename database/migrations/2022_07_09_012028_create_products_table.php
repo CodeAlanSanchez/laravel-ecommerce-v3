d305e3ada517a42cfb310e7d693a455173e185dd
Alan Sanchez <realalansan@gmail.com>
2022-07-09 13:23:51 -0700

    Add migration traits

--- database/migrations/2022_07_09_012028_create_products_table.php
+++ database/migrations/2022_07_09_012028_create_products_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("description");
+            $table->integer("price");
+            $table->string("image_url");
             $table->timestamps();
         });
     }
