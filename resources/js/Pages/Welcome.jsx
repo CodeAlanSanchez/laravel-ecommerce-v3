6d73e6012cd47176badd893a8b1077a9c8b69ecf
Alan Sanchez <realalansan@gmail.com>
2022-07-11 00:40:58 -0700

    Add content to welcome page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -10,8 +10,15 @@ export default function Welcome(props) {
     return (
         <Authenticated auth={props.auth}>
             <Head title="Welcome" />
-            <div className="py-12 px-24">
-                <div className="grid grid-cols-4 gap-4 max-w-7xl mx-auto">
+            <div className="py-12 px-24 max-w-7xl mx-auto">
+                <div className="pb-12 h-72">
+                    <img src="" alt="promotional advertisement" />
+                </div>
+                <div>
+                    <h1 className="text-3xl">Products</h1>
+                    <p className="text-xl font-light text-gray-600 mb-6">Men</p>
+                </div>
+                <div className="grid grid-cols-4 gap-4">
                     {products.length === 0
                         ? '<h1 className="text-2xl">No products found...</h1>'
                         : null}
