40fc872d28a53362d9604a50f81e1cb2d496b166
Alan Sanchez <realalansan@gmail.com>
2022-07-16 11:28:52 -0700

    Addd migration for product fields

---
+++ database/migrations/2022_07_15_231303_add_specifics_products.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('gender')->nullable();
+            $table->integer('discount_price')->nullable();
+            $table->string('clothing_type')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->removeColumn('gender');
+            $table->removeColumn('discount_price');
+        });
+    }
+};
