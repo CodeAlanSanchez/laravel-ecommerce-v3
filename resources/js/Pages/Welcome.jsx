52c5e335eae4c03b82ca557649a18d9c74afead8
Alan Sanchez <realalansan@gmail.com>
2022-07-16 11:31:02 -0700

    Fix responsiveness

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Link, Head } from "@inertiajs/inertia-react";
+import { Head } from "@inertiajs/inertia-react";
 import Authenticated from "../Layouts/Authenticated";
-import { useState } from "react";
 import ProductItem from "../Components/ProductItem";
 
 export default function Welcome(props) {
@@ -10,7 +9,7 @@ export default function Welcome(props) {
     return (
         <Authenticated auth={props.auth}>
             <Head title="Welcome" />
-            <div className="py-12 px-24 max-w-7xl mx-auto">
+            <div className="py-12 sm:px-24 px-4 max-w-7xl mx-auto">
                 <div className="pb-12 h-72">
                     <img src="" alt="promotional advertisement" />
                 </div>
@@ -18,9 +17,9 @@ export default function Welcome(props) {
                     <h1 className="text-3xl">Products</h1>
                     <p className="text-xl font-light text-gray-600 mb-6">Men</p>
                 </div>
-                <div className="grid grid-cols-4 gap-4">
+                <div className="grid text-2xl sm:grid-cols-2 lg:grid-cols-4 lg:text-xl gap-4">
                     {products.length === 0
-                        ? '<h1 className="text-2xl">No products found...</h1>'
+                        ? '<h1 className="text-2xl sm:text-xl">No products found...</h1>'
                         : null}
                     {products.map((p) => (
                         <ProductItem product={p} />
