d018466eabec8b43ad87dca7fa566c9834eee5ab
Alan Sanchez <realalansan@gmail.com>
2022-07-16 19:55:19 -0700

    Add product list to products page

--- resources/js/Pages/Products.jsx
+++ resources/js/Pages/Products.jsx
@@ -1,13 +1,20 @@
-import { Head } from "@inertiajs/inertia-react";
+import { Head, Link } from "@inertiajs/inertia-react";
 import Authenticated from "@/Layouts/Authenticated";
+import ProductList from "../Components/ProductList";
 
 const Products = (props) => {
+    const products = props.products;
+
     return (
         <Authenticated auth={props.auth}>
             <Head title="Products" />
 
-            <div>
-                <h1>Products</h1>
+            <div className="mx-auto max-w-7xl py-16">
+                <h1 className="text-3xl mb-4 inline-block mr-2">Products</h1>{" "}
+                <span className="text-blue-400 hover:underline">
+                    <Link href="/products/create">Create</Link>
+                </span>
+                <ProductList products={products} />
             </div>
         </Authenticated>
     );
