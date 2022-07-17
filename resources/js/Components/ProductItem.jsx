02ffa5f77d35942632801925fb8d9f785588255f
Alan Sanchez <realalansan@gmail.com>
2022-07-16 19:54:22 -0700

    Fix image url

--- resources/js/Components/ProductItem.jsx
+++ resources/js/Components/ProductItem.jsx
@@ -4,7 +4,11 @@ export default function ProductItem({ product }) {
             href={`/product/${product.id}`}
             className="p-2 shadow h-80 bg-white hover:cursor-pointer flex flex-col justify-between"
         >
-            <img src={product.image_url} alt={product.name} className="h-max" />
+            <img
+                src={`storage/${product.image_url}`}
+                alt={product.name}
+                className="h-max"
+            />
             <div>
                 <h4 className="text-black text-lg">{product.name}</h4>
                 <p className="text-red-700 text-lg">${product.price / 100}</p>
