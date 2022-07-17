e6620a1b95edc466d586d54331ee47092572997c
Alan Sanchez <realalansan@gmail.com>
2022-07-16 19:55:13 -0700

    Remove title from product list component

--- resources/js/Components/ProductList.jsx
+++ resources/js/Components/ProductList.jsx
@@ -1,10 +1,8 @@
-export default function (props) {
+import ProductItem from "./ProductItem";
+
+export default function ({ products, filter }) {
     return (
         <div>
-            <div>
-                <h1 className="text-3xl">Products</h1>
-                <p className="text-xl font-light text-gray-600 mb-6">Men</p>
-            </div>
             <div className="grid grid-cols-4 gap-4">
                 {products.length === 0
                     ? '<h1 className="text-2xl">No products found...</h1>'
