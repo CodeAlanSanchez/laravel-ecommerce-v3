d48200584c981f648a2bc7624060e9fe2a4fec8c
Alan Sanchez <realalansan@gmail.com>
2022-07-19 20:48:19 -0700

    Fix image size

--- resources/js/Components/ProductItem.jsx
+++ resources/js/Components/ProductItem.jsx
@@ -1,13 +1,13 @@
 export default function ProductItem({ product }) {
     return (
         <a
-            href={`/product/${product.id}`}
+            href={`/products/${product.id}`}
             className="p-2 shadow h-80 bg-white hover:cursor-pointer flex flex-col justify-between"
         >
             <img
                 src={`storage/${product.image_url}`}
                 alt={product.name}
-                className="h-max"
+                className="object-contain max-h-56"
             />
             <div>
                 <h4 className="text-black text-lg">{product.name}</h4>
