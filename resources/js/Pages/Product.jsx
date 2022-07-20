decf29f11aee07acde6e425ec9cc07dc8eb3a247
Alan Sanchez <realalansan@gmail.com>
2022-07-19 23:08:22 -0700

    Make picture and info same size

--- resources/js/Pages/Product.jsx
+++ resources/js/Pages/Product.jsx
@@ -10,13 +10,13 @@ export default function Product(props) {
             <Head title="Product"></Head>
             <div className="p-4">
                 <div className="md:mx-auto max-w-7xl shadow flex flex-col md:flex-row mt-16 bg-white">
-                    <div>
+                    <div className="flex-1">
                         <img
                             src={`/storage/${product.image_url}`}
                             alt={product.name}
                         />
                     </div>
-                    <div className="p-8 border-x-0 border-b-0 md:border-y-0 md:border-0 md:border-l-2 border-t-gray-100 border-2">
+                    <div className="p-8 border-x-0 border-b-0 md:border-y-0 md:border-0 md:border-l-2 border-t-gray-100 border-2 flex-1">
                         <h1 className="text-2xl mb-1">{product.name}</h1>
                         {product.discounted_price ? (
                             <>
