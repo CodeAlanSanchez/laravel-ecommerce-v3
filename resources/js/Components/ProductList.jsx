600cc75b0049112d2ba0f6d16793da0f69679cb6
Alan Sanchez <realalansan@gmail.com>
2022-07-23 06:58:18 -0700

    Fix product list null check

--- resources/js/Components/ProductList.jsx
+++ resources/js/Components/ProductList.jsx
@@ -4,9 +4,11 @@ export default function ({ products, filter }) {
     return (
         <div>
             <div className="grid text-2xl sm:grid-cols-2 lg:grid-cols-4 lg:text-xl gap-4">
-                {products.length === 0
-                    ? '<h1 className="text-2xl">No products found...</h1>'
-                    : null}
+                {products.length === 0 ? (
+                    <p className="text-md text-gray-600 font-light">
+                        No products found...
+                    </p>
+                ) : null}
                 {products.map((p) => (
                     <ProductItem product={p} />
                 ))}
