28695ebba06282406444dd903f9a7f525298a1c2
Alan Sanchez <realalansan@gmail.com>
2022-07-24 05:26:24 -0700

    Add empty cart state

--- resources/js/Components/CartList.jsx
+++ resources/js/Components/CartList.jsx
@@ -4,6 +4,11 @@ export default function ({ cartItems }) {
     return (
         <div>
             <div className="">
+                {cartItems.lenght > 0 ? (
+                    ""
+                ) : (
+                    <h1 className="text-xl pt-4">Your cart is empty...</h1>
+                )}
                 {cartItems.map((i) => (
                     <CartItem key={i.id} item={i} />
                 ))}
