19a2e53a7a282d951b9054c4f6ed24b0f8615e6c
Alan Sanchez <realalansan@gmail.com>
2022-07-24 05:10:13 -0700

    Add validation rule and remove declaration

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -12,16 +12,15 @@ class CartController extends Controller
 {
     public function addProduct($request)
     {
-
         $validated = $request->validate([
-            'product_id' => 'numeric',
-            'cart_id' => 'numeric',
-            'amount' => 'numeric',
+            'product_id' => 'required|numeric',
+            'cart_id' => 'required|numeric',
+            'amount' => 'required|numeric',
         ]);
 
         $cart = Cart::findOrFail($validated['cart_id']);
 
-        $cartProduct = $cart->cartProduct()->create([
+        $cart->cartProduct()->create([
             'cart_id' => $validated['cart_id'],
             'product_id' => $validated['product_id'],
             'amount' => $validated['amount'],
