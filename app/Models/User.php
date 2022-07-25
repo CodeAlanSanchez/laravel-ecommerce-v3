e55f4a871aea950b165b9e21c3af846985c2c1fd
Alan Sanchez <realalansan@gmail.com>
2022-07-24 20:27:26 -0700

    Fix cart relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,8 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function carts()
+    public function cart()
     {
-        return $this->hasMany(Cart::class);
+        return $this->hasOne(Cart::class);
     }
 }
