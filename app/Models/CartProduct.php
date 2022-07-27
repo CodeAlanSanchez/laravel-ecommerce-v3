befedcab4934eefb62ea71db85f0845c85ff30fc
Alan Sanchez <realalansan@gmail.com>
2022-07-27 12:01:46 -0700

    Fix intermediate table issues

--- app/Models/CartProduct.php
+++ app/Models/CartProduct.php
@@ -3,15 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class CartProduct extends Model
+class CartProduct extends Pivot
 {
     use HasFactory;
+
+    protected $table = 'cart_products';
 
     protected $fillable = [
         'amount'
     ];
+
+    public $incrementing = true;
 
     public function cart()
     {
