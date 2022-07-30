cd65dd0bbcf577aabfa35b517ee2c8b6badf3da3
Alan Sanchez <realalansan@gmail.com>
2022-07-29 23:48:03 -0700

    Fix length typo

--- resources/js/Components/CartList.jsx
+++ resources/js/Components/CartList.jsx
@@ -4,7 +4,7 @@ export default function ({ cartItems }) {
     return (
         <div>
             <div className="">
-                {cartItems.lenght > 0 ? (
+                {cartItems.length > 0 ? (
                     ""
                 ) : (
                     <h1 className="text-xl pt-4">Your cart is empty...</h1>
