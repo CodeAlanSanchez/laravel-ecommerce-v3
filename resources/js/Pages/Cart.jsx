12d40df4100482e3187d184f54d593b2ddabb89f
Alan Sanchez <realalansan@gmail.com>
2022-07-29 21:49:37 -0700

    Pass through cart items

--- resources/js/Pages/Cart.jsx
+++ resources/js/Pages/Cart.jsx
@@ -4,6 +4,7 @@ import CartList from "../Components/CartList";
 
 export default function (props) {
     const auth = props.auth;
+    const cartItems = props.cart_items;
 
     return (
         <Authenticated auth={auth}>
@@ -13,7 +14,7 @@ export default function (props) {
                 <h1 className="text-3xl mb-4">Cart</h1>
                 <div className="bg-white p-4 shadow">
                     <h4 className="text-xl pb-2 border-b-2">Your Items</h4>
-                    <CartList cartItems={[]} />
+                    <CartList cartItems={cartItems} />
                 </div>
             </div>
         </Authenticated>
