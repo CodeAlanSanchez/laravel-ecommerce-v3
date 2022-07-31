079f06e5aa9bd6cf47ab881aa66067d8e85d56ee
Alan Sanchez <realalansan@gmail.com>
2022-07-31 03:46:48 -0700

    Format code

--- resources/js/Pages/Cart.jsx
+++ resources/js/Pages/Cart.jsx
@@ -8,13 +8,13 @@ export default function (props) {
 
     return (
         <Authenticated auth={auth}>
-            <Head title="Cart" />
-
+            <Head title="Cart"/>
+            
             <div className="py-12 sm:px-24 px-4 max-w-7xl mx-auto">
                 <h1 className="text-3xl mb-4">Cart</h1>
                 <div className="bg-white p-4 shadow">
                     <h4 className="text-xl pb-2 border-b-2">Your Items</h4>
-                    <CartList cartItems={cartItems} />
+                    <CartList cartItems={cartItems}/>
                 </div>
             </div>
         </Authenticated>
