2c7287b0bcf25cc27d956b507d4e9daa9f496e20
Alan Sanchez <realalansan@gmail.com>
2022-07-31 11:11:09 -0700

    Add filter links for gender

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import { Head } from "@inertiajs/inertia-react";
-import Authenticated from "../Layouts/Authenticated";
-import ProductList from "../Components/ProductList";
+import Authenticated from "@/Layouts/Authenticated";
+import ProductList from "@/Components/ProductList";
+import FilterDot from "@/Components/FilterDot";
 
 export default function Welcome(props) {
     const products = props.products;
+    const filters = [
+        { url: "/storage/image/men", name: "male" },
+        { url: "/storage/image/women", name: "female" },
+    ];
 
     return (
         <Authenticated auth={props.auth}>
-            <Head title="Welcome" />
+            <Head title="Welcome"/>
+
             <div className="py-12 sm:px-24 px-4 max-w-7xl mx-auto">
-                <div className="pb-12 h-72 ">
-                    <img
-                        src="/storage/image/banner.png"
-                        alt="promotional advertisement"
-                        className="w-full max-h-full"
-                    />
+                <div
+                    className="pb-12 flex justify-center sm:justify-around align-middle flex-col sm:flex-row w-full">
+                    {filters.map(i =>
+                        <FilterDot key={i.name} url={`gender/${i.name}`} image={i.url} name={i.name}/>
+                    )}
                 </div>
                 <div>
-                    <h1 className="text-3xl">Products</h1>
-                    <p className="text-xl font-light text-gray-600 mb-6">Men</p>
+                    <h1 className="text-3xl mb-4">Products</h1>
                 </div>
-                <ProductList products={products} />
+                <ProductList products={products}/>
             </div>
         </Authenticated>
     );
