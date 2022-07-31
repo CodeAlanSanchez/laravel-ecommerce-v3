d0b2f34c8850b8a7e6c3f24847e0d75422c1b9d8
Alan Sanchez <realalansan@gmail.com>
2022-07-31 11:10:20 -0700

    Add gender below title

--- resources/js/Pages/Products.jsx
+++ resources/js/Pages/Products.jsx
@@ -1,20 +1,23 @@
 import { Head, Link } from "@inertiajs/inertia-react";
 import Authenticated from "@/Layouts/Authenticated";
-import ProductList from "../Components/ProductList";
+import ProductList from "@/Components/ProductList";
 
 const Products = (props) => {
     const products = props.products;
+    const gender = props.gender;
 
     return (
         <Authenticated auth={props.auth}>
-            <Head title="Products" />
+            <Head title="Products"/>
 
             <div className="py-12 sm:px-24 px-4 max-w-7xl mx-auto">
-                <h1 className="text-3xl mb-4 inline-block mr-2">Products</h1>{" "}
+                <h1 className="text-3xl inline-block mr-2">Products</h1>{" "}
                 <span className="text-blue-400 hover:underline">
                     <Link href="/products/create">Create</Link>
                 </span>
-                <ProductList products={products} />
+                {gender ? <p className="text-xl font-light text-gray-600 mb-6 capitalize">{gender}</p> :
+                    <div className="mb-4"/>}
+                <ProductList products={products}/>
             </div>
         </Authenticated>
     );
