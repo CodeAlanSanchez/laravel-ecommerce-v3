9ce4f5d8b1474e40273bfbadd4868df3338cf7ea
Alan <realalansan@gmail.com>
2022-08-14 16:03:23 -0700

    Fix guest email

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
 
         $user = User::create([
             "name" => "guest",
-            "email" => "guest",
+            "email" => "guest@guest.com",
             "password" => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'remember_token' => Str::random(10),
         ]);
