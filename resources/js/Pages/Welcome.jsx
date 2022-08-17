85b11e4c327f2e5487d22214949f81b2871d0a11
Alan <realalansan@gmail.com>
2022-08-17 16:02:07 -0700

    Show trending products on front page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -3,9 +3,11 @@ import { Head } from "@inertiajs/inertia-react";
 import Authenticated from "@/Layouts/Authenticated";
 import ProductList from "@/Components/ProductList";
 import FilterDot from "@/Components/FilterDot";
+import { useEffect } from "react";
 
 export default function Welcome(props) {
     const products = props.products;
+    const trending = props.trending;
     const filters = [
         { url: "/storage/image/men", name: "male" },
         { url: "/storage/image/women", name: "female" },
@@ -13,19 +15,20 @@ export default function Welcome(props) {
 
     return (
         <Authenticated auth={props.auth}>
-            <Head title="Welcome"/>
+            <Head title="Welcome" />
 
             <div className="py-12 sm:px-24 px-4 max-w-7xl mx-auto">
-                <div
-                    className="pb-12 flex justify-center sm:justify-around align-middle flex-col sm:flex-row w-full">
-                    {filters.map(i =>
-                        <FilterDot key={i.name} url={`gender/${i.name}`} image={i.url} name={i.name}/>
-                    )}
+                <div className="pb-12 flex justify-center sm:justify-around align-middle flex-col sm:flex-row w-full">
+                    {filters.map((i) => (
+                        <FilterDot
+                            key={i.name}
+                            url={`gender/${i.name}`}
+                            image={i.url}
+                            name={i.name}
+                        />
+                    ))}
                 </div>
-                <div>
-                    <h1 className="text-3xl mb-4">Products</h1>
-                </div>
-                <ProductList products={products}/>
+                <ProductList trending={trending} products={products} />
             </div>
         </Authenticated>
     );
