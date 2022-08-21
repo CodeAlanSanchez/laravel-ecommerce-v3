f07e0b2e8a53a427fb645d96dd926ee832e5f74d
Alan <realalansan@gmail.com>
2022-08-20 22:19:55 -0700

    Add favorite status automatically

--- resources/js/Components/ProductItem.jsx
+++ resources/js/Components/ProductItem.jsx
@@ -1,10 +1,16 @@
-import { Inertia } from "@inertiajs/inertia";
+import axios from "axios";
 import { useState } from "react";
 
 export default function ProductItem({ product }) {
+    const [isFavorite, setIsFavorite] = useState(product.favorite.length > 0);
+
     return (
         <div>
-            <Heart id={product.id} />
+            <Heart
+                favorite={isFavorite}
+                setFavorite={setIsFavorite}
+                id={product.id}
+            />
             <a
                 href={`/products/${product.id}`}
                 className="p-2 shadow h-80 bg-white hover:cursor-pointer flex flex-col justify-between"
@@ -25,27 +31,31 @@ export default function ProductItem({ product }) {
     );
 }
 
-function Heart({ id, favorite = false }) {
-    const [isFavorite, _] = useState(favorite);
-
-    const handleFavorite = () => {
-        Inertia.put(`/products/${id}/favorite`, {
-            favorite: favorite ? 1 : -1,
-        });
+function Heart({ id, favorite = false, setFavorite }) {
+    const handleFavorite = (e) => {
+        axios
+            .put(`/products/${id}/favorite`, {
+                favorite: favorite ? -1 : 1,
+            })
+            .then((r) =>
+                r.data.favorite ? setFavorite(true) : setFavorite(false)
+            );
     };
 
     return (
         <button
             className="mt-2 ml-2 absolute z-10"
-            onClick={() => handleFavorite()}
+            onClick={(e) => handleFavorite(e)}
         >
             <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-6 hover:fill-pink-500"
+                className="w-6"
                 viewBox="0 0 512 512"
             >
                 <path
-                    className="stroke-[20px] stroke-black hover:stroke-pink-500 fill-transparent hover:fill-pink-500"
+                    className={`stroke-[20px] stroke-black hover:stroke-red-500 fill-transparent hover:fill-red-500
+                        ${favorite && "fill-red-500 stroke-red-500"}
+                    `}
                     d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"
                 />
             </svg>
