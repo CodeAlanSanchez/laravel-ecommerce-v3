d30cbd0cafcc5fa659ca43d6d97a5d82af82457c
Alan <realalansan@gmail.com>
2022-10-15 06:10:54 -0700

    Add index to controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Orders', ['orders' => Auth::user()->orders()]);
+    }
 }
