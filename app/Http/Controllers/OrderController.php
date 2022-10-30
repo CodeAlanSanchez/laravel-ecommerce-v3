22b9bed819d2328834ddbcf2555bc5e1cf7d49f2
Alan <realalansan@gmail.com>
2022-10-29 20:23:57 -0700

    Create store method

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\OrderStoreRequest;
+use App\Models\Cart;
+use App\Models\Order;
 use Auth;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -11,5 +14,21 @@ class OrderController extends Controller
     public function index()
     {
         return Inertia::render('Orders', ['orders' => Auth::user()->orders()]);
+    }
+
+    public function store(OrderStoreRequest $request)
+    {
+        $data = $request->validated();
+
+        $products = Cart::first($data['cart_id'])->cartProducts();
+
+        $order = Auth::user()->orders()->create();
+
+        foreach ($products as $product) {
+            $product->cart = $order;
+            $product->save();
+        }
+
+        return redirect('products');
     }
 }
