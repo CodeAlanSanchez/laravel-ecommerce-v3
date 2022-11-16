818bd4772bc78d3ab8434e69e376f8fd149802cd
Alan <realalansan@gmail.com>
2022-11-16 07:36:55 -0800

    Make price field nullable

--- database/migrations/2022_10_14_211501_create_orders_table.php
+++ database/migrations/2022_10_14_211501_create_orders_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
 
             $table->foreignId('user_id');
-            $table->float('price');
+            $table->float('price')->nullable();
 
             $table->timestamps();
         });
