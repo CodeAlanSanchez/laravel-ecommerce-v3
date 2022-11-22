4a960ba07f6180e60ed0419d8f0ca6ab526e186b
Alan <realalansan@gmail.com>
2022-11-22 11:54:15 -0800

    Create search bar component

---
+++ resources/js/Components/Search.jsx
@@ -0,0 +1,41 @@
+import Input from "@/Components/Input";
+import { Icon } from "@iconify/react";
+import { useForm } from "@inertiajs/inertia-react";
+
+export default (props) => {
+    const { data, setData, post } = useForm({ query: "" });
+
+    function onHandleChange(event) {
+        setData(
+            event.target.name,
+            event.target.type === "checkbox"
+                ? event.target.checked
+                : event.target.value
+        );
+    }
+
+    function handleSubmit() {
+        post();
+    }
+
+    return (
+        <form onSubmit={() => handleSubmit()} className="flex content-center">
+            <Input
+                name="query"
+                type="text"
+                value={data.name}
+                placeholder="Search for keywords, brands, or apparel..."
+                className="block w-full after:text-red-400 text-black rounded-r-none"
+                autoComplete="query"
+                handleChange={onHandleChange}
+            />
+            <button type="submit" className="bg-gray-300 px-2 rounded-r">
+                <Icon
+                    icon="material-symbols:search-rounded"
+                    color="black"
+                    width={30}
+                />
+            </button>
+        </form>
+    );
+};
