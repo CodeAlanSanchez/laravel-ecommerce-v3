b289908f3c5ae5525afdd29d7aa5b9ad950199cb
Alan <realalansan@gmail.com>
2022-11-22 11:54:01 -0800

    Add placeholder prop

--- resources/js/Components/Input.jsx
+++ resources/js/Components/Input.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 export default function Input({
-    type = 'text',
+    type = "text",
     name,
     value,
     className,
@@ -9,6 +9,7 @@ export default function Input({
     required,
     isFocused,
     handleChange,
+    placeholder,
 }) {
     const input = useRef();
 
@@ -28,6 +29,7 @@ export default function Input({
                     `border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm ` +
                     className
                 }
+                placeholder={placeholder}
                 ref={input}
                 autoComplete={autoComplete}
                 required={required}
