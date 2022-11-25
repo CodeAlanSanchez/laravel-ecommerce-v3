4065e5b63dc69a2184ef9c627618a8497f20fde6
Alan <realalansan@gmail.com>
2022-11-25 12:27:19 -0800

    Create product analytics component

---
+++ resources/js/Components/ProductAnalytics.jsx
@@ -0,0 +1,23 @@
+export default ({ analytics }) => {
+    return (
+        <div>
+            <hr className="mt-8" />
+            <h2 className="text-2xl mt-8 inline-block mr-4">Analytics</h2>
+            <span className="text-gray-500">Only visible to you</span>
+            <div className="flex flex-row w-full md:w-3/6 justify-between mt-8">
+                <div>
+                    <h4 className="text-2xl font-medium">Views</h4>
+                    <p className="text-xl">{analytics.views}</p>
+                </div>
+                <div>
+                    <h4 className="text-2xl font-medium">Likes</h4>
+                    <p className="text-xl">{analytics.favorites}</p>
+                </div>
+                <div>
+                    <h4 className="text-2xl font-medium">Carts</h4>
+                    <p className="text-xl">{analytics?.cart_adds}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
