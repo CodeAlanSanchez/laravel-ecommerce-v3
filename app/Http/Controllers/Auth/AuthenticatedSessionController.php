cbe2de7cb724e827f14f740ba980d291703cbcf7
Alan <realalansan@gmail.com>
2022-11-27 15:38:35 -0800

    Redirect login to products instead of dashboard

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -37,7 +37,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect('/dashboard');
+        return redirect('/products');
     }
 
     /**
