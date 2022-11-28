126f09c75c5e6aeaf7c6ea95b3f52d73c8b6a6c2
Alan <realalansan@gmail.com>
2022-11-27 20:38:34 -0800

    Fix checkbox component

--- resources/js/Components/Checkbox.jsx
+++ resources/js/Components/Checkbox.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-export default function Checkbox({ name, value, handleChange }) {
+export default function Checkbox({ name, value, handleChange, className }) {
     return (
         <input
             type="checkbox"
             name={name}
-            value={value}
-            className="rounded border-gray-300 text-indigo-600 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            checked={value}
+            className={
+                "rounded border-gray-300 text-indigo-600 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 " +
+                className
+            }
             onChange={(e) => handleChange(e)}
         />
     );
